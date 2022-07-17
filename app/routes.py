93502fa188e5279180cbb6d5c597ca63581f9142
Henry Spivey <Henry Spivey>
2022-07-17 14:21:21 -0700

    added templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
+from flask import render_template
 from app import app 
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world"
+    user = {"username": "Henry"}
+    posts = [
+        {'author': {'username': 'john'},
+        'body': "Beautiful day in portland"},
+        {'author': {'username': 'henry'},
+        'body': "He's makin' jewelry now"}
+    ]
+    return render_template('index.html', title=None, user=user, posts=posts)
