f7358f3a93400f4eb451a8d5a5aad2f0a98f18d7
Henry Spivey <Henry Spivey>
2022-07-31 21:59:05 -0700

    adding next page, user login

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
-from flask import flash, redirect, render_template, url_for
+from flask import flash, redirect, render_template, url_for, request
 from app import app 
 from app.forms import LoginForm
+from flask_login import current_user, login_required, login_user, logout_user
+from app.models import User
+from werkzeug.urls import url_parse
+
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {"username": "Henry"}
     posts = [
@@ -16,8 +21,22 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated():
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Login requested for user {}, remember={}'.format(form.username.data, form.remember_me.data))
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page =  url_for('index')
+        return redirect(url_for(next_page))
     return render_template('login.html', title="Sign In", form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
