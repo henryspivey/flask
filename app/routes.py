c787d5da822f5167acb01df0ddef38b253bd0ae4
Henry Spivey <henryspivey2@gmail.com>
2022-09-17 16:06:23 -0700

    add new fields to user model, edit profile route, modified profile template, added before_request decorator

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,18 @@
 from flask import flash, redirect, render_template, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_required, login_user, logout_user
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+
+# insert code that I want to execute before any view function in the application,
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 
 @app.route("/")
@@ -33,6 +42,7 @@ def login():
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get("next")
+        print(next_page)
         if not next_page or url_parse(next_page).netloc != "":
             next_page = url_for("index")
         return redirect(url_for(next_page[1:]))
@@ -58,3 +68,31 @@ def register():
         flash("Congratulations, you are now a registered user!")
         return redirect(url_for("login"))
     return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {"author": user, "body": "Test post #1"},
+        {"author": user, "body": "Test post #2"},
+    ]
+    return render_template("user.html", user=user, posts=posts)
+
+
+@app.route("/edit_profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved.")
+        return redirect(url_for("edit_profile"))
+
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", title="Edit Profile", form=form)
