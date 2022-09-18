14bb28b5a6332036addd5667a75d7740777dcaa9
Henry Spivey <henryspivey2@gmail.com>
2022-09-17 17:16:09 -0700

    added custom 404, 500 errors, modified user profile route to check for existing username

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,57 @@
+from distutils.command.config import config
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate 
+from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 login = LoginManager(app)
-login.login_view  = 'login' # used in url_for calls
+login.login_view = "login"  # used in url_for calls
 
-from app import routes, models
+from app import routes, models, errors
+from logging.handlers import RotatingFileHandler
+import os
+
+# creates a SMTPHandler instance, sets its level so that it only reports errors and not warnings, informational or debugging messages, and finally attaches it to the app.logger object from Flask.
+
+
+if not app.debug:
+    if app.config["MAIL_SERVER"]:
+        auth = None
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+            auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+        secure = None
+        if app.config["MAIL_USE_TLS"]:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+            fromaddr="no-replay@" + app.config["MAIL_SERVER"],
+            toaddrs=app.config["ADMINS"],
+            subject="Microblog failure",
+            credentials=auth,
+            secure=secure,
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+        file_handler = RotatingFileHandler(
+            "logs/microblog.log", maxBytes=10240, backupCount=10
+        )
+        file_handler.setFormatter(
+            logging.Formatter(
+                "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+            )
+        )
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info("Microblog startup")
